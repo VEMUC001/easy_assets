7da0388254ae400b6c4a85d6a2460f0535ea9b9a
VEMUC001 <chandra.vemulapalli@disney.com>
2024-10-05 00:40:49 -0700

    first working implementation

---
+++ example/lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:easy_assets/easy_assets.dart';
+import 'generated/assets.dart'; // This will be generated
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // Preload assets
+  await AssetLoader().preloadAssets([
+    Assets.images_flutter_logo_png,
+  ]);
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'EasyAssets Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        fontFamily: 'OpenSans',
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('EasyAssets Example'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset(
+              AssetLoader().getAssetPath(Assets.images_flutter_logo_png),
+              width: 200,
+            ),
+            SizedBox(height: 20),
+            Text(
+              'This image was loaded using EasyAssets',
+              style: TextStyle(fontSize: 16),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
